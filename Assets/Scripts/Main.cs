92aceabcdd82afb59b9d320213978929e0a675d7
ConsoleWarrior <oleg.minichev@yandex.ru>
2022-07-29 16:27:29 +0300

    29.07 work

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -10,13 +10,16 @@ public class Main : MonoBehaviour
     public float delay;
     private IEnumerator corot;
     [SerializeField] Text bitcoin;
-    public static float kredit = 250;
+    [SerializeField] Text timerr;
+    public static float kredit = 10000;
     private float timer = 0;
     private float alltimer = 0;
+    public GameObject youwin;
 
     IEnumerator CreatorLeft(){
         while(true){
             if (timer > 60 && delay > 1) {delay -= 0.5f; timer = 0;}
+            if(timer >60 && delay > 0.5 && delay <= 1) { delay -= 0.2f; timer = 0; }
             Instantiate(Resources.Load("Ant", typeof(Ant)), new Vector2(UnityEngine.Random.Range(-10,-20), UnityEngine.Random.Range(-10,10)), Quaternion.identity);
             yield return new WaitForSeconds(delay);// рандомный спавн UnityEngine.Random.Range(0.1f,4f)
             timer += delay; alltimer += delay;
@@ -30,6 +33,12 @@ public class Main : MonoBehaviour
             Instantiate(Resources.Load("Ant", typeof(Ant)), new Vector2(UnityEngine.Random.Range(10, 20), UnityEngine.Random.Range(-10, 10)), Quaternion.identity);
             yield return new WaitForSeconds(delay);// рандомный спавн UnityEngine.Random.Range(0.1f,4f)
             //timer += delay;
+            if (alltimer > 480f && alltimer < 481f)
+            {
+                Instantiate(Resources.Load("Crab", typeof(Crab)), new Vector2(UnityEngine.Random.Range(10, 20), UnityEngine.Random.Range(-10, 10)), Quaternion.identity);
+                Instantiate(Resources.Load("Crab", typeof(Crab)), new Vector2(UnityEngine.Random.Range(-10, -20), UnityEngine.Random.Range(-10, 10)), Quaternion.identity);
+                //alltimer = 0;
+            }
         }
     }
 
@@ -49,10 +58,8 @@ public class Main : MonoBehaviour
         kredit += Time.deltaTime * 1f; //прирост битков
         int a = (int)Math.Round(kredit);
         bitcoin.text = a.ToString();
-        if(alltimer >= 360){
-            Instantiate(Resources.Load("Crab", typeof(Crab)), new Vector2(UnityEngine.Random.Range(10, 20), UnityEngine.Random.Range(-10, 10)), Quaternion.identity);
-            Instantiate(Resources.Load("Crab", typeof(Crab)), new Vector2(UnityEngine.Random.Range(-10, -20), UnityEngine.Random.Range(-10, 10)), Quaternion.identity);
-            alltimer = 0;
-        }
+        timerr.text = alltimer.ToString();
+        
+        if (alltimer >= 600) youwin.SetActive(true);
     }
 }
